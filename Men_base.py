3460ec756346e2eb93ff6a02e6847d59101b05dc
constanza <co.barrag@duocuc.cl>
2025-06-23 11:35:02 -0400

    Update Men_base.py

--- Men_base.py
+++ Men_base.py
@@ -1,3 +1,5 @@
+def datos_constanza():
+    print("mi nombre es constanza y tengo 19 años")
 # Menú base del programa
 while True:
     print("\n--- MENÚ PRINCIPAL ---")
@@ -10,7 +12,7 @@ while True:
         print("Programa finalizado.")
         break
     elif op == "1":
-        pass # Aquí se llamará a la función del integrante 1
+        datos_constanza():
     elif op == "2":
         pass # Aquí se llamará a la función del integrante 2
     elif op == "3":
