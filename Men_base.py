b734c489183b28b2bb93666e3c0e7631005d87e2
constanza <co.barrag@duocuc.cl>
2025-06-23 11:58:16 -0400

    Update Men_base.py

--- Men_base.py
+++ Men_base.py
@@ -13,7 +13,7 @@ while True:
         print("Programa finalizado.")
         break
     elif op == "1":
-        datos_constanza():
+        datos_constanza()
     elif op == "2":
         pass # Aquí se llamará a la función del integrante 2
     elif op == "3":
