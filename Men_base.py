b986d910af39aae115dd6a6e105c59399883bfe5
Etániel <eta.rapu@duocuc.cl>
2025-06-25 12:08:31 -0400

    Update Men_base.py

--- Men_base.py
+++ Men_base.py
@@ -23,6 +23,8 @@ while True:
     elif op == "2":
         pass # Aquí se llamará a la función del integrante 2
     elif op == "3":
-        datos_valentina ():
+        pass # Aquí se llamará a la función del integrante 3
+    elif op == "4":
+        pass # Aquí se llamará a la función del integrante 4
     else:
         print(" Opción inválida.")  
