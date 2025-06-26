021ee2a0c78e7d24b78c8296517002fc6b0a55f6
Etániel <eta.rapu@duocuc.cl>
2025-06-26 12:09:04 -0600

    Update Men_base.py

--- Men_base.py
+++ Men_base.py
@@ -1,5 +1,5 @@
-def datos_constanza():
-    print("mi nombre es constanza y tengo 19 años")
+
+
 
 
 
@@ -19,7 +19,7 @@ while True:
         print("Programa finalizado.")
         break
     elif op == "1":
-        datos_constanza()
+        pass # Aquí se llamará a la función del integrante 2
     elif op == "2":
         pass # Aquí se llamará a la función del integrante 2
     elif op == "3":
